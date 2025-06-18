a45ca7c30dfd173dc8d19f7cb6949d46a99ef5f8
Matt Wonlaw <mlaw@roci.dev>
2025-06-18 11:05:19 -0400

    welcome to zocs

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -30,7 +30,7 @@ export default function LoginPage() {
       <div className="max-w-md w-full space-y-8 p-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Welcome to Zotion
+            Welcome to Zocs
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Please sign in to continue
